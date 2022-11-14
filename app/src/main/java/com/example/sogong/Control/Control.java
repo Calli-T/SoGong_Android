30b2cb120339a855132dfb03d77725c15bde490b
joy14 <joy14479@gmail.com>
2022-11-14 11:40:06 +0900

    Control 클래스의 인터페이스로의 변환

--- app/src/main/java/com/example/sogong/Control/Control.java
+++ app/src/main/java/com/example/sogong/Control/Control.java
@@ -7,6 +7,15 @@ import com.example.sogong.View.LoginActivity;
 
 import java.util.List;
 
+public interface Control {
+    public abstract void startToast(String message);
+    public abstract void startDialog(int type, String title, String message, List<String> btnTxtList);
+    public abstract void changePage(String nowPage, String pageName);
+}
+
+/* The origin
+import java.util.List;
+
 public class Control {
     public void startToast(String message){
     }
@@ -20,3 +29,5 @@ public class Control {
 
     }
 }
+
+ */
