be38a26538cc087b827e561cf8467418d325111f
Calli-T <joy14479@gmail.com>
2022-11-17 21:27:27 +0900

    플래그로 스레드 제어

--- app/src/main/java/com/example/sogong/View/RecipeFragment.java
+++ app/src/main/java/com/example/sogong/View/RecipeFragment.java
@@ -22,16 +22,17 @@ import java.util.List;
 
 public class RecipeFragment extends Fragment {
 
-    String[] pagenum = {"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24","25","26","27","28","29","30","31","32","33","34","35","36","37","38","39","40"};
+    String[] pagenum = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40"};
     public static int totalpage;
     public static List<RecipePost> list = null;
     public static int responseCode = 0;
+    private boolean threadFlag; // 프래그먼트 전환에서 스레드를 잠재울 플래그
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        View view = inflater.inflate(R.layout.fragment_recipeboard,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_recipeboard, container, false);
         Spinner spinner1 = view.findViewById(R.id.recipe_page_spinner);
-        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_spinner_item,pagenum);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, pagenum);
         adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner1.setAdapter(adapter1);
         spinner1.setPrompt("이동할 페이지");
@@ -42,9 +43,14 @@ public class RecipeFragment extends Fragment {
         RecipeList_UI rlu = new RecipeList_UI();
 
         // t.start까지 코드는 시작하자마자 불러오는 레시피 리스트
+        threadFlag = true;
+
         final Runnable runnable = new Runnable() {
+            int tt = 0;
+
             @Override
             public void run() {
+                rlu.startToast("" + (++tt));
                 if (responseCode == 200) {
                     responseCode = -1;
                 } else {
@@ -62,7 +68,11 @@ public class RecipeFragment extends Fragment {
                         e.printStackTrace();
                     }
 
-                    getActivity().runOnUiThread(runnable);
+                    if (threadFlag)
+                        getActivity().runOnUiThread(runnable);
+                    else {
+                        i = 30;
+                    }
                 }
             }
         }
@@ -72,8 +82,14 @@ public class RecipeFragment extends Fragment {
         NewRunnable nr = new NewRunnable();
         Thread t = new Thread(nr);
         t.start();
-        
+
         return view;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        threadFlag = false;
     }
 
     class RecipeList_UI implements Control {
